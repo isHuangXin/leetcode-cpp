015f7485b276e2b2804732f748d8f75cf3605dfb
Xin Huang <597934012@qq.com>
2022-01-17 10:24:15 +0800

    2022.01.17 add 06.reversePrint in JZoffer

---
+++ 00.the-sword-to-offer/06.reversePrint.cpp
@@ -0,0 +1,37 @@
+//
+// Created by huangxin on 1/17/22.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+private:
+    stack<int> temp;
+    vector<int> res;
+    ListNode* cur;
+public:
+    vector<int> reversePrint(ListNode* head) {
+        cur = head;
+        while (cur){
+            temp.push(cur->val);
+            cur = cur->next;
+        }
+        while (!temp.empty()){
+            res.push_back(temp.top());
+            temp.pop();
+        }
+        return res;
+    }
+};
