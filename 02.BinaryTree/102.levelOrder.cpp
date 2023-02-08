cd382179e6b73631ea2a3432ed2634b255d84872
isHuangXin <597934012@qq.com>
2023-02-08 20:25:48 +0800

    BinaryTree: levelOrder

---
+++ 02.BinaryTree/102.levelOrder.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Problem Link
+// https://leetcode.cn/problems/binary-tree-level-order-traversal/
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root == nullptr) {
+            return res;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int currentLevelSize = q.size();
+            vector<int> layer;
+            for (int i = 1; i <= currentLevelSize; i++) {
+                auto node = q.front();
+                q.pop();
+                layer.push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            res.push_back(layer);
+        }
+        return res;
+    }
+};
