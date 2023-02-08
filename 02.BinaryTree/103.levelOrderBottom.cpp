8237955a01b796fe3b9c7fdeff0d1cd302c39cac
isHuangXin <597934012@qq.com>
2023-02-08 21:04:31 +0800

    BinaryTree: 107.levelOrderBottom

---
+++ 02.BinaryTree/103.levelOrderBottom.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Problem 103
+// Link: https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/solution/
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> bottomBfs;
+        vector<vector<int>> bfs;
+        if (root == nullptr) {
+            return bottomBfs;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int currentLevelSize = q.size();
+            vector<int> layer;
+            for (int i = 0; i < currentLevelSize; i++) {
+                auto node = q.front();
+                q.pop();
+                layer.push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            bfs.push_back(layer);
+        }
+
+        for (int i = 0; i < bfs.size(); i++) {
+            bottomBfs.push_back(bfs[bfs.size()-1-i]);
+        }
+        return bottomBfs;
+    }
+};
