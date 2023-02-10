a7fe74c8a8a257178692bc5ed031a623f1b039d1
isHuangXin <597934012@qq.com>
2023-02-10 17:35:03 +0800

    Hahs: 242.isAnagram

---
+++ 03.Hash/242.isAnagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // 思路1: 字母比较少，数组映射效率比较高
+        // 思路2: 直接哈希
+        if(s.size() != t.size()) {
+            return false;
+        }
+        unordered_map<char, int> hashMapS, hashMapT;
+        for(const auto &i : s) {
+            ++hashMapS[i];
+        }
+        for(const auto &j : t) {
+            ++hashMapT[j];
+        }
+        if (hashMapS == hashMapT) {
+            return true;
+        }
+        return false;
+    }
+};
