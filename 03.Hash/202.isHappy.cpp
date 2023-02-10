4092efab472ccea232269bdb6f719f69faea1c66
isHuangXin <597934012@qq.com>
2023-02-10 21:49:50 +0800

    Hash: 202.isHappy

---
+++ 03.Hash/202.isHappy.cpp
@@ -0,0 +1,30 @@
+// Problem: 202.快乐数
+// Link: https://leetcode.cn/problems/happy-number/
+class Solution {
+public:
+    int sum(int n) {
+        int sum = 0;
+        while(n) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> res;
+        while(1) {
+            int num = sum(n);
+            if (num == 1) {
+                return true;
+            }
+            if (res.find(num) != res.end()) {
+                return false;
+            } else {
+                // unordered_set 添加元素用 .emplace() 方法
+                res.emplace(num);
+            }
+            n = num;
+        }
+    }
+};
