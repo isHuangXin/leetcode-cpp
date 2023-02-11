94c563b081d45b982b4abd9f2becbdf9846a32d7
isHuangXin <597934012@qq.com>
2023-02-11 09:13:13 +0800

    Hash: 1.twoSum

---
+++ 03.Hash/1.twoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // 思路1: 双重循环
+        // 思路2: 哈希map
+        unordered_map<int, int> hashtable;
+        for(int i=0; i<nums.size(); i++) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
