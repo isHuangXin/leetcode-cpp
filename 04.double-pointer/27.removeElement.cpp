f8f140ecf7a914c3e4f2d9091de77b3c7921a756
isHuangXin <597934012@qq.com>
2023-02-12 10:26:01 +0800

    DoublePointer: 27.removeElement

---
+++ 04.double-pointer/27.removeElement.cpp
@@ -0,0 +1,16 @@
+// Problem: 27.移除元素
+// Link: https://leetcode.cn/problems/remove-element/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // 双指针法：左右指针最初在同一位置
+        int left = 0;
+        for(int right=0; right<nums.size(); right++) {
+            if(nums[right]!=val) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+};
