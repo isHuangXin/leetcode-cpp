639a21ee0f279cf93ae4bebef2b10ffd9cf841d1
isHuangXin <597934012@qq.com>
2023-02-14 22:00:52 +0800

    DoublePointer: 151.reverseWords

---
+++ 04.DoublePointer/151.reverseWords.cpp
@@ -0,0 +1,37 @@
+// Problem: 151. 反转字符串
+// Link: https://leetcode.cn/problems/reverse-words-in-a-string/
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left <= right && s[left] == ' ') {
+            left++;
+        }
+        while (right >= left && s[right] == ' ') {
+            right--;
+        }
+        deque<string> d;
+        string word;
+        while (left <= right) {
+            char c = s[left];
+            if (word.size() && c == ' ') {
+                d.push_front(word);
+                word = "";
+            } else if (c != ' ') {
+                word += c;
+            }
+            left++;
+        }
+        d.push_front(word);
+
+        string ans;
+        while (!d.empty()) {
+            ans += d.front();
+            d.pop_front();
+            if (!d.empty()) {
+                ans += ' ';
+            }
+        }
+        return ans;
+    }
+};
