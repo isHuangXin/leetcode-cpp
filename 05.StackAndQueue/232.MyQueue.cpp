7907963327d8c75643c575ef4e9505560b00764f
isHuangXin <597934012@qq.com>
2023-03-12 16:34:37 +0800

    StackAndQueue: 232.MyQueue

---
+++ 05.StackAndQueue/232.MyQueue.cpp
@@ -0,0 +1,52 @@
+// Problem: 232.用栈实现队列
+// Link: https://leetcode.cn/problems/implement-queue-using-stacks/
+class MyQueue {
+private:
+    stack<int> inStack, outStack;
+
+    void in2out() {
+        while(!inStack.empty()) {
+            outStack.push(inStack.top());
+            inStack.pop();
+        }
+    }
+
+public:
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        inStack.push(x);
+    }
+    
+    int pop() {
+        if(outStack.empty()) {
+            in2out();
+        }
+        int x = outStack.top();
+        outStack.pop();
+        return x;
+    }
+    
+    int peek() {
+        if(outStack.empty()) {
+            in2out();
+        }
+        int x = outStack.top();
+        return x;
+    }
+    
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+        // return (outStack.empty() && inStack.empty());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
